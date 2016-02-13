e409f1045a9148291e757a236953d23001ed20a0
Pascal Vree <pvree@mirabeau.nl>
2016-02-13 16:14:36 +0100

    refactoring: renamed config.json file to config.aws.s3.json

--- facade.js
+++ facade.js
@@ -1,5 +1,5 @@
 var service = require('./service.js');
-var partialConfig = require('./config.json');
+var partialConfig = require('./config.aws.s3.json');
 
 var Store = function(suppliedConfig) {
   var completeConfig = partialConfig;
