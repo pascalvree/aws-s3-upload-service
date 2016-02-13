f1373bb611f6484730d213c9009c9ef67c98a7a3
Pascal Vree <pvree@mirabeau.nl>
2016-02-13 17:03:00 +0100

    Introduced unique id for example upload

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+var uuid = require('uuid');
 var config = require(process.env.CONFIGFILE || '../config/config.aws.json');
 var controller = require('./controller.js');
 
 controller.Store(
   config,
   {
-    key: '1.txt',
+    key: uuid.v1(),
     bucket: 'aws-s3-store',
     filepath: 'aant/1.txt',
     serverSideEncryption: 'AES256'
